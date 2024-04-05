8f7818273d24fe2667018aa0f9839b3d054078fd
Gururajj77 <gururajtakeshi@gmail.com>
2024-04-05 14:08:35 +0530

    added env file

---
+++ src/environment/environment.ts
@@ -0,0 +1,1 @@
+export const firebaseConfig = { "projectId": "edzeeta-certification", "appId": "1:389921084048:web:64bd90334f47104ae7be15", "storageBucket": "edzeeta-certification.appspot.com", "apiKey": "AIzaSyDpku5wPzt_XsFSWlbt2F20FlVdUWuP1kE", "authDomain": "edzeeta-certification.firebaseapp.com", "messagingSenderId": "389921084048", "measurementId": "G-5YTMBJQ92Y" }
